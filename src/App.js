97ac1d676e20005349828334afbc6e549df7e71d
Walace-Costa <walacec453@gmail.com>
2026-07-11 14:32:52 -0300

    Auto-sync: 2026-07-11 17:32:52 UTC

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './index.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -8,8 +10,24 @@ import Mencoes from './components/Mencoes';
 import Politica from './components/Politica';
 import Contato from './components/Contato';
 import Footer from './components/Footer';
+import Galeria from './components/Galeria';
 
-export default function App() {
+function ScrollToHash() {
+  const location = useLocation();
+  useEffect(() => {
+    if (location.hash) {
+      const el = document.getElementById(location.hash.slice(1));
+      if (el) {
+        setTimeout(() => el.scrollIntoView({ behavior: 'smooth' }), 60);
+        return;
+      }
+    }
+    window.scrollTo(0, 0);
+  }, [location]);
+  return null;
+}
+
+function Home() {
   return (
     <div>
       <Navbar />
@@ -22,5 +40,17 @@ export default function App() {
       <Contato />
       <Footer />
     </div>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToHash />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/galeria" element={<Galeria />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
