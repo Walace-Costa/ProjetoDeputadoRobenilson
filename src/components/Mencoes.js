107142332a0feae785bf1392666846807f59b884
Walace-Costa <walacec453@gmail.com>
2026-07-15 22:43:19 -0300

    Auto-sync: 2026-07-16 01:43:19 UTC

--- src/components/Mencoes.js
+++ src/components/Mencoes.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { useMediaQuery } from '../hooks/useMediaQuery';
